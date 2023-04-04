53029fe915a69dc5fb9369e3904f5aecd3ed7473
Jingning Zhang <55093876+Jingning-Zhang@users.noreply.github.com>
2023-04-04 17:05:50 -0400

    Update and rename prsepr_5eth.sh to prosper_5eth.sh

--- Simulation_final/computation_time/prosper_5eth.sh
+++ Simulation_final/computation_time/prosper_5eth.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#$ -N prsepr_5eth
+#$ -N prosper_5eth
 #$ -cwd
 #$ -l mem_free=3G,h_vmem=3G,h_fsize=100G,h='compute-119'
 #$ -m e
@@ -8,9 +8,9 @@
 
 module load conda_R/4.0
 
-mkdir /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prsepr/5pop/
+mkdir /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prosper/5pop/
 
-mkdir /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prsepr/5pop/$SGE_TASK_ID
+mkdir /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prosper/5pop/$SGE_TASK_ID
 
 cd /dcs04/nilanjan/data/jzhang2/MEPRS/codes/pacakge
 
@@ -19,7 +19,7 @@ date '+%A %W %Y %X'
 
 Rscript lassosum2.R \
 --PATH_package /dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/PRS-epr \
---PATH_out /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prsepr/5pop/$SGE_TASK_ID/lassosum2 \
+--PATH_out /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prosper/5pop/$SGE_TASK_ID/lassosum2 \
 --PATH_plink /dcs04/nilanjan/data/jzhang2/TOOLS/plink/plink2 \
 --FILE_sst /dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/example/summdata/EUR.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/example/summdata/AFR.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/example/summdata/AMR.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/example/summdata/EAS.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/example/summdata/SAS.txt \
 --pop EUR,AFR,AMR,EAS,SAS \
@@ -31,16 +31,16 @@ Rscript lassosum2.R \
 
 Rscript PRS-epr.R \
 --PATH_package /dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/PRS-epr \
---PATH_out /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prsepr/5pop/$SGE_TASK_ID/PRSepr \
+--PATH_out /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prosper/5pop/$SGE_TASK_ID/prosper \
 --FILE_sst /dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/example/summdata/EUR.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/example/summdata/AFR.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/example/summdata/AMR.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/example/summdata/EAS.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/example/summdata/SAS.txt \
 --pop EUR,AFR,AMR,EAS,SAS \
 --chrom 22 \
---lassosum_param /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prsepr/5pop/$SGE_TASK_ID/lassosum2/EUR/optimal_param.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prsepr/5pop/$SGE_TASK_ID/lassosum2/AFR/optimal_param.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prsepr/5pop/$SGE_TASK_ID/lassosum2/AMR/optimal_param.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prsepr/5pop/$SGE_TASK_ID/lassosum2/EAS/optimal_param.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prsepr/5pop/$SGE_TASK_ID/lassosum2/SAS/optimal_param.txt \
+--lassosum_param /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prosper/5pop/$SGE_TASK_ID/lassosum2/EUR/optimal_param.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prosper/5pop/$SGE_TASK_ID/lassosum2/AFR/optimal_param.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prosper/5pop/$SGE_TASK_ID/lassosum2/AMR/optimal_param.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prosper/5pop/$SGE_TASK_ID/lassosum2/EAS/optimal_param.txt,/dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prosper/5pop/$SGE_TASK_ID/lassosum2/SAS/optimal_param.txt \
 --NCORES 1
 
 Rscript tuning_testing.R \
 --PATH_plink /dcs04/nilanjan/data/jzhang2/TOOLS/plink/plink2 \
---PATH_out /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prsepr/5pop/$SGE_TASK_ID/PRSepr \
+--PATH_out /dcs04/nilanjan/data/jzhang2/MEPRS/computation_time/prosper/5pop/$SGE_TASK_ID/prosper \
 --prefix AFR \
 --testing F \
 --bfile_tuning /dcs04/nilanjan/data/jzhang2/MEPRS/pacakge/example/sample_data/AFR/tuning_geno \
